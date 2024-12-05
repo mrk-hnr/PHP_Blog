a90441a99b788c465312d398ec11a8df5db3c3fc
mrk-hnr <mrk.hnr@hotmail.com>
2024-12-05 17:00:06 +0800

    Added PHP Script - Posts under specific category reverts to Others when the category is deleted

--- admin/category-delete.php
+++ admin/category-delete.php
@@ -9,22 +9,16 @@ if (isset($_GET["id"])) {
     $id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);
     // FETCH DATA FROM DB
 
-    $query = "SELECT * FROM categories WHERE id = $id";
-    $result = mysqli_query($connection, $query);
-    $category = mysqli_fetch_assoc($result);
+    $update_query = "UPDATE posts SET category_id = 8 WHERE category_id = $id";
+    $update_result = mysqli_query($connection, $update_query);
 
-
-
-    // DELETE USER FROM DB
-    $delete_category_query = "DELETE FROM categories WHERE id = $id";
-    $delete_category_result = mysqli_query($connection, $delete_category_query);
-
-    if (mysqli_errno($connection)) {
-        $_SESSION["delete-category"] = "Unable to delete category {$category['title']}";
-    } else {
+    if (!mysqli_errno($connection)) {
+        $query = "DELETE FROM categories WHERE id = $id LIMIT 1";
+        $result = mysqli_query($connection, $delete_category_query);
         $_SESSION["delete-category-success"] = "Category Deleted - {$category['title']}";
+    } else {
+        $_SESSION["delete-category"] = "Unable to delete category {$category['title']}";
     }
-
 }
 
 header("Location: " . ROOT_URL . "admin/manage-category.php");
